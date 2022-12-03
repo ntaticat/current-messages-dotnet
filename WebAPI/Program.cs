77a01294be0daba7ad10b61c17b542a6580511e6
Rafael Estrada <ntaticat@gmail.com>
2022-12-03 17:41:45 -0600

    update swaggerui config

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -47,7 +47,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CurrentMessagesNet V1");
+        c.RoutePrefix = "";
+    });
 }
 
 // app.UseHttpsRedirection();
